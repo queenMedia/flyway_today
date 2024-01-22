0e0486aa40147f6d745afe2981149b0f6981fa98
Guillermo Fragoso Delgadillo <sigfried-dev@pop-os.localdomain>
2024-01-22 15:12:42 -0600

    :sparkles: add data file for text

--- src/ui/sharing/organisms/Footer/Footer.jsx
+++ src/ui/sharing/organisms/Footer/Footer.jsx
@@ -1,9 +1,16 @@
 import React from "react"
 import * as styles from "./Footer.style"
 import Logo from "@images/sharing/logo.webp"
+import { data } from "@data/footer"
 import { Button, Mail, Address } from "@sharing/atoms"
 
 export default function Footer() {
+  const { title_email, 
+    email, 
+    title_address, 
+    address, 
+    button,
+    copy } = data
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
@@ -19,27 +26,27 @@ export default function Footer() {
           <div className={styles.contact_container("item2")}>
             <div className={styles.text_container}>
               <Mail />
-              <p className={styles.title}>Correo electrónico</p>
+              <p className={styles.title}>{title_email}</p>
             </div>
-            <p className={styles.text}>soporte@flywaytoday.com</p>
+            <p className={styles.text}>{email}</p>
           </div>
 
           <div className={styles.contact_container("item3")}>
             <div className={styles.text_container}>
               <Address />
-              <p className={styles.title}>Dirección</p>
+              <p className={styles.title}>{title_address}</p>
             </div>
-            <p className={styles.text}>IGNACIO ALLENDE 21 AMPLIACION TORRE BLANCA 11289 MIGUEL HIDALGO, CDMX</p>
+            <p className={styles.text}>{address}</p>
           </div>
           <Button 
             types='submit'
-            text="Inscribirme"
+            text={button}
             style={styles.button}
             action={''}
           />
         </div>
         <p className={styles.text_bottom}>
-          FlyWay 2023 All Right Reserved
+          {copy}
         </p>
       </div>      
     </footer>
